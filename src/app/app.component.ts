5beb4f51241d40032bb86ff46d06e309c9f16435
Andre Santini <andresantini123@gmail.com>
2018-01-17 15:25:03 -0200

    test 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,8 @@ export class MyApp {
   rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  statusBar.styleDefault();
+   splashScreen.hide();
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
